10e86264c42933c10c2bff4a9d9cee7711794185
amzhulanov <amzhulanov@ya.ru>
2019-03-28 01:19:43 +0700

    Изменены текстовые сообщения Исправлены методы преодоления препятствий

--- src/Java/ru/geekbrains/lesson1/animal/Dog.java
+++ src/Java/ru/geekbrains/lesson1/animal/Dog.java
@@ -5,8 +5,15 @@ import Java.ru.geekbrains.lesson1.enums.Color;
 
 public class Dog extends Animal implements Participant {
 
-    public Dog(String name, Color color, int age) {
+    private boolean isOnDistance;
+    private int runDistance;
+    private int jumpHeight;
+
+    public Dog(String name, Color color, int age, int runDistance, int jumpHeight) {
         super(name, color, age);
+        this.isOnDistance=true;
+        this.runDistance=runDistance;
+        this.jumpHeight=jumpHeight;
     }
 
     public Dog(String name, Color color) {
@@ -20,21 +27,80 @@ public class Dog extends Animal implements Participant {
 
     @Override
     public boolean isOnDistance() {
-        return false; // TODO доработать по аналогии с классом Cat
+
+        return isOnDistance;
     }
 
     @Override
     public void run(int distance) {
-        // TODO доработать по аналогии с классом Cat
+        if (!isOnDistance) {
+            return;
+        }
+        if (distance > runDistance) {
+            isOnDistance = false;
+            if (gender){
+                System.out.println(String.format("Собака %s не хватило сил пробежать %d и она снимается с соревнований. На прошлых соревнованиях она достигла отметки %d", getName(), distance,runDistance));
+            }else{
+                System.out.println(String.format("Псу %s не хватило сил пробежать %d и он снимается с соревнований. На прошлых соревнованиях он достиг отметки %d", getName(), distance,runDistance));
+            }
+            return;
+        }
+        //Если последний символ из имени животного гласный, то женский род.
+        if (gender) {
+            System.out.println(String.format("Собака %s пробежала кросс длинной %d. Её личный рекорд %d", getName(), distance,runDistance));
+        }else {
+            System.out.println(String.format("Пёс %s пробежал кросс длинной %d. Его личный рекорд %d", getName(), distance,runDistance));
+        }
     }
 
     @Override
     public void jump(int height) {
-        // TODO доработать по аналогии с классом Cat
+        if (!isOnDistance) {
+            return;
+        }
+        if (height > jumpHeight) {
+            isOnDistance = false;
+            if (gender) {
+                System.out.println(String.format("Собака %s не смогла взять высоту %d и она снимается с соревнований" , getName(), height));
+            }else {
+                System.out.println(String.format("Пёс %s не смог взять высоту %d и он снимается с соревнований" , getName(), height));
+            }
+            return;
+        }
+        if (gender) {
+            System.out.println(String.format("Собака %s прыгнула на высоту %d. Её личный рекорд %d", getName(), height, jumpHeight));
+        }else {
+            System.out.println(String.format("Пёс %s прыгнул на высоту %d. Его личный рекорд %d", getName(), height, jumpHeight));
+        }
     }
 
     @Override
     public void swim(int distance) {
-        // TODO доработать по аналогии с классом Cat
+        if (!isOnDistance) {
+            return;
+        }
+        if (distance > runDistance) {
+            isOnDistance = false;
+            if (gender) {
+                System.out.println(String.format("Собака %s не кошка, но всё равно не смогла проплыть %d и снимается с соревнований.",getName(),distance));
+            }else{
+                System.out.println(String.format("Пёс %s не кот, но всё равно не смог проплыть %d и снимается с соревнований.",getName(),distance));
+            }
+            return;
+        }
+        if (gender) {
+            System.out.println(String.format("Собака %s не кошка и смогла проплыть %d. Её личный рекорд %d.",getName(),distance,runDistance));
+        }else{
+            System.out.println(String.format("Пёс %s не кот и смог проплыть %d. Его личный рекорд %d.",getName(),distance,runDistance));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "isOnDistance=" + isOnDistance +
+                ", runDistance=" + runDistance +
+                ", jumpHeight=" + jumpHeight +
+                '}';
     }
 }
