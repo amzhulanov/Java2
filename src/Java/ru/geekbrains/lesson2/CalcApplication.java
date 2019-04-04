578b5939df1540b7b33c6037e4c9d029045e79ce
amzhulanov <amzhulanov@ya.ru>
2019-04-04 19:08:44 +0700

    Метод проверки наличия символа в массиве перенёс в основной класс. В main оставил цикл для повтора программы и дополнительное исключение для контроля ввода ответа. Размерность массива указывается в переменной dimensionArray

--- src/Java/ru/geekbrains/lesson2/CalcApplication.java
+++ src/Java/ru/geekbrains/lesson2/CalcApplication.java
@@ -2,53 +2,51 @@ package Java.ru.geekbrains.lesson2;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import static Java.ru.geekbrains.lesson2.EditArray.*;
 
-import static Java.ru.geekbrains.lesson2.EditArray.checkNumber;
-import static Java.ru.geekbrains.lesson2.EditArray.readArray;
-import static Java.ru.geekbrains.lesson2.EditArray.writeArray;
 
 public class CalcApplication {
+    public static int positionX=0;
+    public static int positionY=0;
 
     public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
-        int ext=1;//переменная для выхода из цикла
-        int numMassiv=0;
-        try{Scanner scn=new Scanner(System.in);
-        do {
-            System.out.println("Выберите размерность заполнения массива от 1 до 4.");
-            numMassiv = scn.nextInt();
-           // if (numMassiv != 4) {
-            //    throw new MyArraySizeException("Размерность массива ограничена 4х4. Вы указали " + numMassiv+". Программа завершает свою работу.");
-            //} else {
+        int ext = 1;//переменная для выхода из цикла
 
+        Scanner scn = new Scanner(System.in);
+        try {
+
+            do {
                 System.out.println("Заполняю массив случайными числами.");
+                fillArrayWithRandomNumbers();//заполняю массив случайными числами и одним символом
+                if (myArray.length != 4) {
+                    throw new MyArraySizeException(String.format("Размерность массива ограничена 4х4. Длина вашего массива %s.", myArray.length));
+                }
+                printArray();//Вывожу на экран сформированный массив
 
-                writeArray(numMassiv);
-                readArray(numMassiv);
-                System.out.println("Сумма чисел в массиве = " + checkNumber());
-                System.out.println("Ещё разок ? 1-Да   0-Нет");
-                ext=scn.nextInt();
-            //}
-        }
+                try{
+                    System.out.println("Сумма чисел в массиве = " + checkCharacterInArray());//Здесь я проверяю наличие не числа в массиве и суммирую все ячейки.
 
-        while (ext==1);
-        } catch  (NumberFormatException ex) {
-            System.out.println("Ошибка преобразования числа " + ex+". Программа завершает свою работу.");
-        }catch (InputMismatchException ex){
+                }catch (NumberFormatException ex) {
+                    throw new MyArrayDataException(String.format("В строке %s и столбце %s найден символ, отличный от числа. Программа завершает свою работу.",positionX+1,positionY+1));
+                }
+                System.out.println("Ещё разок ? 1-Да   0-Нет");//завершаю цикл, если ответ не равен 1
+                ext = scn.nextInt();
+            }
+            while (ext == 1);
+        }catch (InputMismatchException ex) {
             System.out.println("Введено неверное число, будьте аккуратнее. Программа завершает свою работу.");
-        }catch (IndexOutOfBoundsException ex){
-            throw new MyArraySizeException("Вы нарушили правила. Размерность массива ограничена 4х4. Вы указали " + numMassiv+". Программа завершает свою работу.");
         }
-
-
     }
 
-
-
-
-
-
-
-
+    public static int checkCharacterInArray() {//метод для проверки массива и вычисления суммы
+        int sum=0;
+        for ( positionX = 0; positionX <4 ; positionX++) {
+            for ( positionY = 0; positionY <4 ; positionY++) {
+                sum=sum+Integer.valueOf(myArray[positionX][positionY]);
+            }
+        }
+        return sum;
+    }
 
 }
 
--- src/Java/ru/geekbrains/lesson2/EditArray.java
+++ src/Java/ru/geekbrains/lesson2/EditArray.java
@@ -1,13 +1,15 @@
 package Java.ru.geekbrains.lesson2;
 
- public class EditArray {
-    static String[][] myArray=new String[4][4];
+import java.util.Scanner;
+
+public class EditArray {
+     private static int dimensionArray=4;//размерность массива
+     static String[][] myArray=new String[dimensionArray][dimensionArray];
 
 
-     public static void writeArray(int dimension) {//метод для наполнения массива случайными числами и символом "а"
-
-            for (int i = 0; i < dimension; i++) {
-                for (int j = 0; j < dimension; j++) {
+     public static void fillArrayWithRandomNumbers() {//метод для наполнения массива случайными числами и символом "а"
+            for (int i = 0; i < dimensionArray; i++) {
+                for (int j = 0; j < dimensionArray; j++) {
                     if (j == (int) (Math.random() * 20)) {
                         myArray[i][j] = "a";
                     } else {
@@ -18,30 +20,15 @@ package Java.ru.geekbrains.lesson2;
             }
         }
 
-    public  static void readArray(int dimension) {//метод для отображения массива
+    public  static void printArray() {//метод для отображения массива
 
-        for (int i = 0; i < dimension; i++) {
-            for (int j = 0; j < dimension; j++) {
+        for (int i = 0; i < dimensionArray; i++) {
+            for (int j = 0; j < dimensionArray; j++) {
                 System.out.print(myArray[i][j] + ":");
             }
             System.out.println();
         }
     }
 
-    public static int checkNumber() throws MyArrayDataException {//метод для проверки массива и вычисления суммы
-        //int ch;
-        int i=0;
-        int j=0;
-        int sum=0;
-        try{
-            for ( i = 0; i <4 ; i++) {
-                for ( j = 0; j <4 ; j++) {
-                    sum=sum+Integer.valueOf(myArray[i][j]);
-               }
-            }
-        }catch (NumberFormatException ex)
-                {throw new MyArrayDataException(String.format("Что-то пошло не так. В строке %s и столбце %s найден символ, отличный от числа. Программа завершает свою работу.",i+1,j+1));
-        }
-         return sum;
-    }
+
 }
