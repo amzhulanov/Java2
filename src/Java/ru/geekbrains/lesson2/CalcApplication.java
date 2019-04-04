21d514ade9ed37d1ed232437cf3dfd6960293fa1
amzhulanov <amzhulanov@ya.ru>
2019-04-04 17:39:32 +0700

    Исключение MyArraySizeException перенёс в catch. Старое местоположение просто закомментировал.

--- src/Java/ru/geekbrains/lesson2/CalcApplication.java
+++ src/Java/ru/geekbrains/lesson2/CalcApplication.java
@@ -11,29 +11,32 @@ public class CalcApplication {
 
     public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
         int ext=1;//переменная для выхода из цикла
+        int numMassiv=0;
         try{Scanner scn=new Scanner(System.in);
         do {
             System.out.println("Выберите размерность заполнения массива от 1 до 4.");
-            int numMassiv = scn.nextInt();
-            if (numMassiv != 4) {
-                throw new MyArraySizeException("Размерность массива ограничена 4х4. Вы указали " + numMassiv+". Программа завершает свою работу.");
-            } else {
+            numMassiv = scn.nextInt();
+           // if (numMassiv != 4) {
+            //    throw new MyArraySizeException("Размерность массива ограничена 4х4. Вы указали " + numMassiv+". Программа завершает свою работу.");
+            //} else {
 
                 System.out.println("Заполняю массив случайными числами.");
+
                 writeArray(numMassiv);
                 readArray(numMassiv);
                 System.out.println("Сумма чисел в массиве = " + checkNumber());
                 System.out.println("Ещё разок ? 1-Да   0-Нет");
                 ext=scn.nextInt();
-            }
+            //}
         }
+
         while (ext==1);
         } catch  (NumberFormatException ex) {
             System.out.println("Ошибка преобразования числа " + ex+". Программа завершает свою работу.");
         }catch (InputMismatchException ex){
             System.out.println("Введено неверное число. Программа завершает свою работу.");
         }catch (IndexOutOfBoundsException ex){
-            System.out.println("Массив ограничен размерностью 4. Программа завершает свою работу.");
+            throw new MyArraySizeException("Вы нарушили правила. Размерность массива ограничена 4х4. Вы указали " + numMassiv+". Программа завершает свою работу.");
         }
 
 
