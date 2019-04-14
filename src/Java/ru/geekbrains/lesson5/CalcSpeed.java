4d18d72281cc1127ea4ffee7058403233065c00f
amzhulanov <amzhulanov@ya.ru>
2019-04-14 16:48:05 +0700

    Добавил заполнение исходного массива дефолтными значениями, после обработки однопотоковым методом. Убрал сквозную адресацию, а для корректности расчёта математической формулы сделал (n+h)

--- src/Java/ru/geekbrains/lesson5/CalcSpeed.java
+++ src/Java/ru/geekbrains/lesson5/CalcSpeed.java
@@ -9,6 +9,7 @@ public class CalcSpeed {
     public static void main(String[] args)throws InterruptedException {
         setDefaultValue();
         System.out.println(String.format("Обработка массива одним потоком длилась - %d ms", speedCalc()));
+        setDefaultValue();
         System.out.println(String.format("Обработка массива двумя потоками длилась - %d ms", speedMultiThreading()));
 
 
--- src/Java/ru/geekbrains/lesson5/MultiThreading.java
+++ src/Java/ru/geekbrains/lesson5/MultiThreading.java
@@ -4,24 +4,24 @@ import static Java.ru.geekbrains.lesson5.OneThread.*;
 
 public class MultiThreading {
     private static float[] arrMultiThread1 = new float[h];
-    private static float[] arrMultiThread2 = new float[size];
+    private static float[] arrMultiThread2 = new float[h];
     long a = System.currentTimeMillis();
     private static volatile boolean waitThread1, waitThread2 = false;
 
     public static long speedMultiThreading() throws InterruptedException {
-        long delay,id1,id2=0;
-
-        System.arraycopy(arrOneThread, 0, arrMultiThread1, 0, h);
-        System.arraycopy(arrOneThread, h, arrMultiThread2, h - 1, h);
+        long delay, id1, id2 = 0;
         long a = System.currentTimeMillis();
+        System.arraycopy(arrOneThread, 0, arrMultiThread1, 0, h);
+        System.arraycopy(arrOneThread, h, arrMultiThread2, 0, h);
+
         FirstThread();
         SecondThread();
         while (!waitThread1 || !waitThread2) {
             Thread.currentThread().sleep(10);
         }
         System.arraycopy(arrMultiThread1, 0, arrOneThread, 0, h);
-        System.arraycopy(arrMultiThread2, h - 1, arrOneThread, h, h);
-        delay=System.currentTimeMillis() - a;
+        System.arraycopy(arrMultiThread2, 0, arrOneThread, h, h);
+        delay = System.currentTimeMillis() - a;
         return (delay);
     }
 
@@ -35,7 +35,7 @@ public class MultiThreading {
                     arrMultiThread1[i] = (float) (arrMultiThread1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
                 waitThread1 = true;
-                }
+            }
 
         }.start();
     }
@@ -46,10 +46,10 @@ public class MultiThreading {
             public void run() {
                 for (int n = 0;
                      n < h - 1; n++) {
-                    arrMultiThread2[n] = (float) (arrMultiThread2[n] * Math.sin(0.2f + n/ 5) * Math.cos(0.2f + n / 5) * Math.cos(0.4f + n / 2));
+                    arrMultiThread2[n] = (float) (arrMultiThread2[n] * Math.sin(0.2f + (n +h)/ 5) * Math.cos(0.2f + (n+h) / 5) * Math.cos(0.4f + (n+h) / 2));
                 }
                 waitThread2 = true;
             }
         }.start();
-           }
+    }
 }
