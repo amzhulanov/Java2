7e6a8b26c5bcaedbb5ad66e7ef6adf06121dfbc6
amzhulanov <amzhulanov@ya.ru>
2019-04-14 15:54:17 +0700

    ДЗ выполнено. Создал три класса. CalcSpeed - класс с методом main(). В нём вызывается метод заполнения массива. Обработка массива одним потоком Обработка массива двумя потоками

---
+++ src/Java/ru/geekbrains/lesson5/CalcSpeed.java
@@ -0,0 +1,17 @@
+package Java.ru.geekbrains.lesson5;
+
+import static Java.ru.geekbrains.lesson5.MultiThreading.speedMultiThreading;
+import static Java.ru.geekbrains.lesson5.OneThread.setDefaultValue;
+import static Java.ru.geekbrains.lesson5.OneThread.speedCalc;
+
+public class CalcSpeed {
+
+    public static void main(String[] args)throws InterruptedException {
+        setDefaultValue();
+        System.out.println(String.format("Обработка массива одним потоком длилась - %d ms", speedCalc()));
+        System.out.println(String.format("Обработка массива двумя потоками длилась - %d ms", speedMultiThreading()));
+
+
+    }
+
+}
---
+++ src/Java/ru/geekbrains/lesson5/MultiThreading.java
@@ -0,0 +1,55 @@
+package Java.ru.geekbrains.lesson5;
+
+import static Java.ru.geekbrains.lesson5.OneThread.*;
+
+public class MultiThreading {
+    private static float[] arrMultiThread1 = new float[h];
+    private static float[] arrMultiThread2 = new float[size];
+    long a = System.currentTimeMillis();
+    private static volatile boolean waitThread1, waitThread2 = false;
+
+    public static long speedMultiThreading() throws InterruptedException {
+        long delay,id1,id2=0;
+
+        System.arraycopy(arrOneThread, 0, arrMultiThread1, 0, h);
+        System.arraycopy(arrOneThread, h, arrMultiThread2, h - 1, h);
+        long a = System.currentTimeMillis();
+        FirstThread();
+        SecondThread();
+        while (!waitThread1 || !waitThread2) {
+            Thread.currentThread().sleep(10);
+        }
+        System.arraycopy(arrMultiThread1, 0, arrOneThread, 0, h);
+        System.arraycopy(arrMultiThread2, h - 1, arrOneThread, h, h);
+        delay=System.currentTimeMillis() - a;
+        return (delay);
+    }
+
+    private static void FirstThread() {
+
+        new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0;
+                     i < h - 1; i++) {
+                    arrMultiThread1[i] = (float) (arrMultiThread1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+                waitThread1 = true;
+                }
+
+        }.start();
+    }
+
+    private static void SecondThread() {
+        new Thread() {
+            @Override
+            public void run() {
+                for (int n = 0;
+                     n < h - 1; n++) {
+                    arrMultiThread2[n] = (float) (arrMultiThread2[n] * Math.sin(0.2f + n/ 5) * Math.cos(0.2f + n / 5) * Math.cos(0.4f + n / 2));
+                }
+                waitThread2 = true;
+            }
+        }.start();
+           }
+}
