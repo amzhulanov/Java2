3604807fed37a84c68248f97276b8f3418011327
amzhulanov <amzhulanov@ya.ru>
2019-04-15 16:34:26 +0700

    Убрал дублирование. Удалил один избыточный класс

--- src/Java/ru/geekbrains/lesson5/MultiThreading.java
+++ src/Java/ru/geekbrains/lesson5/MultiThreading.java
@@ -1,50 +1,61 @@
 package Java.ru.geekbrains.lesson5;
 
-import static Java.ru.geekbrains.lesson5.OneThread.*;
-
+import java.util.Arrays;
 
 public class MultiThreading {
-    private static final int h=arrOneThread.length/2;//чтобы константы оставались приватными
+    private static final int size = 10000000;
+    private static final int h = size / 2;
+    private static float[] arrOneThread = new float[size];
     private static float[] arrMultiThread1 = new float[h];
     private static float[] arrMultiThread2 = new float[h];
-    private static final long a = System.currentTimeMillis();
+    private static long a = System.currentTimeMillis();
     private static Thread thread1, thread2;
 
-
-    public static long speedMultiThreading() throws InterruptedException {
-        long id1, id2 = 0;
-        long a = System.currentTimeMillis();
+    public static void speedMultiThreading() throws InterruptedException {
+        //расчёт с одним потоком
+        setDefaultValue();
+        a = System.currentTimeMillis();
+        calcArray(arrOneThread, size, 0);
+        System.out.println(String.format("Обработка массива одним потоком длилась - %d ms", System.currentTimeMillis() - a));
+        //расчёт с двумя потоками
+        setDefaultValue();
+        a = System.currentTimeMillis();
         System.arraycopy(arrOneThread, 0, arrMultiThread1, 0, h);
         System.arraycopy(arrOneThread, h, arrMultiThread2, 0, h);
-
+        //формирую первый поток
         thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                calcArray(arrMultiThread1, 0);
+                calcArray(arrMultiThread1, h, 0);
             }
         });
         thread1.start();
-
+        //формирую второй поток
         thread2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                calcArray(arrMultiThread2, h);
+                calcArray(arrMultiThread2, h, h);
             }
         });
         thread2.start();
-        thread1.join();
+        thread1.join();//ожидаю завершения потоков
         thread2.join();
-
+        //соединяю массивы
         System.arraycopy(arrMultiThread1, 0, arrOneThread, 0, h);
         System.arraycopy(arrMultiThread2, 0, arrOneThread, h, h);
-        return System.currentTimeMillis() - a;
+        System.out.println(String.format("Обработка массива двумя потоками длилась - %d ms", System.currentTimeMillis() - a));
+
     }
 
-    private static void calcArray(float[] arr, int shift) {
-        for (int n = 0;
-             n < h - 1; n++) {
+    private static void calcArray(float[] arr, int sizeArray, int shift) {
+        for (int n = 0; n < sizeArray - 1; n++) {
             arr[n] = (float) (arr[n] * Math.sin(0.2f + (n + shift) / 5) * Math.cos(0.2f + (n + shift) / 5) * Math.cos(0.4f + (n + shift) / 2));
         }
-
     }
+
+    private static void setDefaultValue() {//заполняю массив одинаковыми значениями
+        Arrays.fill(arrOneThread, 1);
+    }
+
+    
 }
