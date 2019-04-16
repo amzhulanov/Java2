92bc059375438774ef21844ddb20d0f034de2554
amzhulanov <amzhulanov@ya.ru>
2019-04-16 14:33:06 +0700

    Доработано ДЗ 4-го урока. Добавлен вывод имени пользователя и времени

--- src/Java/ru/geekbrains/lesson4/HomeWork4/ViewWindow.java
+++ src/Java/ru/geekbrains/lesson4/HomeWork4/ViewWindow.java
@@ -6,32 +6,52 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ViewWindow extends JFrame {
-    private JTextField message = new JTextField();
-    private static DefaultListModel<String> textArea = new DefaultListModel<String>();
+    private final JTextField message;
+    private final TextMessageCellRenderer messageCellRenderer;
+    private final JList<TextMessage> jList;
+    private  final DefaultListModel<TextMessage> jListModel;
+    private final JPanel mainPanel;
+    private final JScrollPane scroll;
+    private final JButton buttonEnter;
 
     public ViewWindow() {
         setTitle("Chat");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setBounds(200, 200, 500, 500);
-        JPanel mainPanel = new JPanel();
-        JButton buttonEnter = new JButton("Отправить");
+        mainPanel = new JPanel();
+         buttonEnter = new JButton("Отправить");
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
-        JList<String> jList = new JList<String>(textArea);
+
+        jList = new JList<>();
+        jListModel = new DefaultListModel<>();
+        messageCellRenderer=new TextMessageCellRenderer();
+        jList.setModel(jListModel);
+        jList.setCellRenderer(messageCellRenderer);
+        message = new JTextField();
+
+
+        scroll=new JScrollPane(jList,
+                        JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                        JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        add(scroll,BorderLayout.CENTER);
+
         ActionListener actionListener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
+                String text=message.getText().trim();
                 if (message.getText().trim().length() > 0) {
-                    textArea.add(textArea.getSize(), message.getText().trim() + "\n");
+                    TextMessage msg=new TextMessage("Вы",text);
+                    jListModel.add(jListModel.getSize(), msg);
+                    message.setText(null);
                 }
-                validate();
-                message.setText("");
+
+
             }
         };
         message.addActionListener(actionListener);
         mainPanel.add(message);
         mainPanel.add(buttonEnter);
         buttonEnter.addActionListener(actionListener);
-        add(new JScrollPane(jList), BorderLayout.CENTER);
         add(mainPanel, BorderLayout.SOUTH);
         setVisible(true);
     }
